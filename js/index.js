f12c187f7efe623c2c445b48fe45f1163e2fab5a
Ayoub Boujida <ayoubboujida@gmail.com>
2021-03-23 11:24:35 +0100

    new commit

--- js/index.js
+++ js/index.js
@@ -1,7 +1,17 @@
 // Iteration 1: Names and Input
-const user = "Ayoub";
+const hacker1 = "jason";
+const hacker2 = "kim";
 
 // Iteration 2: Conditionals
+if(hacker1.length == hacker2.length ) {
+    console.log('same length');
+} else if (hacker1.length < hacker2.length) {
+    console.log('hacker 2 less character)');}
+    else if (hacker1.length > hacker2.length) {
+        console.log('hacker 1 less character');}
+    else {
+        console.log("not at all")};
+
 
 
 // Iteration 3: Loops
